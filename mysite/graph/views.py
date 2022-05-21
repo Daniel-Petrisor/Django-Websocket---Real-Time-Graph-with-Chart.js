5c4236981a98013ac07f625b32a5bf01303b02a5
ZenBook <danielpetrisor@live.it>
2022-05-21 08:57:09 +0200

    Starter project

---
+++ mysite/graph/views.py
@@ -0,0 +1,6 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+def index(request):
+    context = {'text': 'Hello World'}
+    return render(request, 'base.html', context)
